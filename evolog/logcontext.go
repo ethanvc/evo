c571975c8d5c39c343feee75220bbc73ad1f1577
smith <xht_xht@qq.com>
2023-07-27 22:52:47 +0800

    add code

--- evolog/logcontext.go
+++ evolog/logcontext.go
@@ -1,5 +1,14 @@
 package evolog
 
+import "context"
+
 type LogContext struct {
 	TraceId string
 }
+
+func WithLogContext(c context.Context, traceId string) context.Context {
+	lc := &LogContext{TraceId: traceId}
+	return context.WithValue(c, contextKeyLogContext{}, lc)
+}
+
+type contextKeyLogContext struct{}
