59d82f0e2c39ec95192f2efd80f9b778670dd3e3
smith <xht_xht@qq.com>
2023-07-28 09:05:25 +0800

    add code

--- evohttp/server_test.go
+++ evohttp/server_test.go
@@ -2,41 +2,31 @@ package evohttp
 
 import (
 	"context"
-	"fmt"
 	"github.com/stretchr/testify/require"
 	"net/http"
-	"sync/atomic"
 	"testing"
 )
 
 func TestServer_Simple(t *testing.T) {
 	svr := NewServer()
+	listenAddr, url := getTestListenAddr()
+	httpSvr := &http.Server{
+		Handler: svr,
+		Addr:    listenAddr,
+	}
+	go func() {
+		httpSvr.ListenAndServe()
+	}()
+	defer httpSvr.Shutdown(context.Background())
 	test := func(c context.Context, req any, info *RequestInfo) (any, error) {
 		return nil, nil
 	}
-	listenAddr, url := getNextListenAddr()
 	svr.POSTF("/test", test)
-	go func() {
-		svr.Run(listenAddr)
-	}()
 	st := NewSingleAttempt(http.MethodPost, url+"/test")
 	err := st.Invoke(context.Background(), nil, nil)
 	require.NoError(t, err)
 }
 
-var nextPortNumber int32
-
-func getNextListenAddr() (listenAddr string, url string) {
-	const minPort = 15000
-	const maxPort = 16000
-	for {
-		port := atomic.AddInt32(&nextPortNumber, 1)
-		if port < minPort || port > maxPort {
-			atomic.CompareAndSwapInt32(&nextPortNumber, port, minPort)
-			continue
-		}
-		listenAddr = fmt.Sprintf("127.0.0.1:%d", port)
-		url = "http://" + listenAddr
-		return
-	}
+func getTestListenAddr() (listenAddr string, url string) {
+	return "127.0.0.1:9032", "http://127.0.0.1:9032"
 }
