ad606d19773d379108d80a6b089349913cb8fad7
smith <xht_xht@qq.com>
2023-09-15 08:04:58 +0800

    add code

---
+++ evores/cgroup/cgroupv1_test.go
@@ -0,0 +1,24 @@
+package cgroup
+
+import (
+	"fmt"
+	"github.com/dustin/go-humanize"
+	"testing"
+)
+
+func TestControlGroupV1_Basic(t *testing.T) {
+	cg := NewControlGroupV1()
+	cpuCount, _ := cg.CpuCount()
+	cpuUsage, _ := cg.CpuUsageSeconds()
+	memSize, _ := cg.MemorySize()
+	memUsage, _ := cg.MemoryUsageBytes()
+
+	fmt.Printf(`CPU Count: %d
+CPU Usage Seconds: %f
+Memory Size: %s(%d)
+Memory Usage Bytes: %s(%d)
+`, cpuCount, cpuUsage,
+		humanize.Bytes(uint64(memSize)), memSize,
+		humanize.Bytes(uint64(memUsage)), memUsage,
+	)
+}
