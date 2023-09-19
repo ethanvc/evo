19b935f232c08471c6739e0ec1f7fab5993714c4
smith <xht_xht@qq.com>
2023-09-20 07:58:00 +0800

    add code

--- evohttp/example_test.go
+++ evohttp/example_test.go
@@ -11,26 +11,28 @@ import (
 	"net/http/httptest"
 )
 
-func Example_a() {
+func Example_paramInUrl() {
 	svr := NewServer()
 	type GetUserReq struct {
-		UserId int64 `json:"user_id"`
+		UserId int64  `json:"user_id"`
+		Name   string `json:"name"`
 	}
 	type GetUserResp struct {
-		UserId int64 `json:"user_id"`
+		UserId int64  `json:"user_id"`
+		Name   string `json:"name"`
 	}
 	h := func(c context.Context, req *GetUserReq) (resp *GetUserResp, err error) {
 		resp = new(GetUserResp)
 		copier.Copy(resp, req)
 		return
 	}
-	svr.POST("/api/users/:user_id", NewStdHandlerF(h))
+	svr.POST("/api/users/:user_id/:name", NewStdHandlerF(h))
 	req := &GetUserReq{}
 	httpRequest := httptest.NewRequest(http.MethodPost,
-		"/api/users/123", bytes.NewReader(base.AnyToJson(req)))
+		"/api/users/123/jack", bytes.NewReader(base.AnyToJson(req)))
 	httpRecoder := httptest.NewRecorder()
 	svr.ServeHTTP(httpRecoder, httpRequest)
 	result, _ := io.ReadAll(httpRecoder.Body)
 	fmt.Print(string(result))
-	// Output: {"code":0,"msg":"","event":"","data":{"user_id":123}}
+	// Output: {"code":0,"msg":"","event":"","data":{"user_id":123,"name":"jack"}}
 }
