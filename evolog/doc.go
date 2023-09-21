0e372a8dc41c9474945d4178cca686b2c0b309b3
smith <xht_xht@qq.com>
2023-09-21 08:58:34 +0800

    add code

---
+++ evolog/doc.go
@@ -0,0 +1,6 @@
+package evolog
+
+/*
+For RPC or http request, report `evo_server_event_total` when execution finished. The report event format is:
+`REQ:${code}:${event}`, for example: `REQ:NOT_FOUND:UserNotFound`
+*/
