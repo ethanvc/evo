551901d69deb703832bcd78ca64501a130fc6507
smith <xht_xht@qq.com>
2023-09-21 08:21:02 +0800

    add code

---
+++ examples/dns_balance_check/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ethanvc/evo/evolog"
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+	c := evolog.WithLogContext(nil, &evolog.LogContextConfig{Method: "query_dns"})
+	for {
+		ips, err := net.LookupIP("google.com")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Could not get IPs: %v\n", err)
+			time.Sleep(time.Second)
+		}
+		if len(ips) == 0 {
+			fmt.Fprintf(os.Stderr, "no ip found\n")
+			time.Sleep(time.Second)
+		}
+		evolog.DefaultReporter().ReportEvent(c, "ResolveIP:"+ips[0].String())
+	}
+}
