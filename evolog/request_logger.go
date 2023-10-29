15f5bcbf905ee1e9d62446ff3accd6a8592836d0
smith <xht_xht@qq.com>
2023-10-29 21:55:19 +0800

    support filter before log the request

--- evolog/request_logger.go
+++ evolog/request_logger.go
@@ -7,6 +7,7 @@ import (
 import "context"
 
 type RequestLogger struct {
+	filter func(c context.Context, err error, req, resp any) bool
 }
 
 func NewRequestLogger() *RequestLogger {
@@ -15,21 +16,27 @@ func NewRequestLogger() *RequestLogger {
 }
 
 func (rl *RequestLogger) Log(c context.Context, logInfo *RequestLogInfo, extra ...any) {
+	if logInfo == nil {
+		logInfo = &RequestLogInfo{}
+	}
+	if !rl.callFilter(c, logInfo.Err, logInfo.Req, logInfo.Resp) {
+		return
+	}
 	lc := GetLogContext(c)
 	lvl := slog.LevelInfo
 	var args []any
 	args = append(args, slog.String("method", lc.GetMethod()))
-	if logInfo != nil {
-		if logInfo.Err != nil {
-			args = append(args, Error(logInfo.Err))
-		}
-		if logInfo.Req != nil {
-			args = append(args, slog.Any("req", logInfo.Req))
-		}
-		if logInfo.Resp != nil {
-			args = append(args, slog.Any("resp", logInfo.Resp))
-		}
+
+	if logInfo.Err != nil {
+		args = append(args, Error(logInfo.Err))
 	}
+	if logInfo.Req != nil {
+		args = append(args, slog.Any("req", logInfo.Req))
+	}
+	if logInfo.Resp != nil {
+		args = append(args, slog.Any("resp", logInfo.Resp))
+	}
+
 	args = append(args, extra...)
 	args = append(args, slog.Int64("tc", time.Now().Sub(lc.GetStartTime()).Microseconds()))
 	if events := lc.GetEvents(); len(events) > 0 {
@@ -38,8 +45,22 @@ func (rl *RequestLogger) Log(c context.Context, logInfo *RequestLogInfo, extra .
 	Log(c, lvl, 1, "REQ_END", args...)
 }
 
+func (rl *RequestLogger) callFilter(c context.Context, err error, req, resp any) bool {
+	if rl.filter != nil {
+		return rl.filter(c, err, req, resp)
+	} else {
+		return true
+	}
+}
+
 type RequestLogInfo struct {
 	Err  error
 	Req  any
 	Resp any
+}
+
+var defaultRequestLogger = NewRequestLogger()
+
+func DefaultRequestLogger() *RequestLogger {
+	return defaultRequestLogger
 }
