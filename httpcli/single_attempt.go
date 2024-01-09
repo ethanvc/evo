d8c7994c9bb10a3130382b0b3e5f66aa689ca6c2
smith <xht_xht@qq.com>
2024-01-09 21:39:00 +0800

    add code

--- httpcli/single_attempt.go
+++ httpcli/single_attempt.go
@@ -4,8 +4,8 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"errors"
 	"github.com/ethanvc/evo/base"
+	"google.golang.org/grpc/codes"
 	"io"
 	"net/http"
 	"strings"
@@ -14,9 +14,9 @@ import (
 type SingleAttempt struct {
 	Request  *http.Request
 	Response *http.Response
-	Resp     any
-	chain    base.Chain[*SingleAttempt]
 	err      error
+	RespBody []byte
+	Template *HttpTemplate
 }
 
 func NewSingleAttempt(c context.Context, httpMethod, url string) *SingleAttempt {
@@ -24,108 +24,103 @@ func NewSingleAttempt(c context.Context, httpMethod, url string) *SingleAttempt 
 		c = context.Background()
 	}
 	sa := &SingleAttempt{}
-	sa.chain = defaultSingleAttemptInterceptors
+	sa.Template = DefaultTemplate
 	var err error
 	// make error processing easier
 	sa.Request, err = http.NewRequestWithContext(c, httpMethod, url, nil)
 	if err != nil {
-		sa.Request, _ = http.NewRequestWithContext(c, httpMethod, "http://badurl_or_method_offered", nil)
+		sa.err = err
+		sa.Request, _ = http.NewRequestWithContext(c, httpMethod, "http://127.0.0.1:1/badurl_or_method_offered", nil)
 	}
 	return sa
 }
 
 func (sa *SingleAttempt) Do(req any, resp any) (err error) {
-	sa.Resp = resp
-	_, err = sa.chain.Do(sa.Request.Context(), req, sa)
-	return
+	if sa.err != nil {
+		return sa.err
+	}
+	return sa.Template.Do(sa.Request.Context(), req, resp, sa)
 }
 
-type SingleAttemptInterceptor struct {
+func Do[Resp any](sa *SingleAttempt, req any) (Resp, error) {
+	var resp Resp
+	err := sa.Do(req, &resp)
+	return resp, err
 }
 
-func NewSingleAttemptInterceptor() *SingleAttemptInterceptor {
-	return &SingleAttemptInterceptor{}
+var DefaultTemplate *HttpTemplate = NewHttpTemplate()
+
+type HttpTemplate struct {
+	Client  *http.Client
+	Encoder func(ctx context.Context, req any, sa *SingleAttempt) error
+	Decoder func(ctx context.Context, sa *SingleAttempt) error
 }
 
-func (si *SingleAttemptInterceptor) Handle(c context.Context, req any, info *SingleAttempt, nexter base.Nexter[*SingleAttempt]) (resp any, err error) {
-	resp, err = si.preHandle(c, req, info, nexter)
+func NewHttpTemplate() *HttpTemplate {
+	return &HttpTemplate{
+		Client: http.DefaultClient,
+	}
+}
+
+func (template *HttpTemplate) Do(c context.Context, req, resp any, sa *SingleAttempt) error {
+	err := template.encoder(c, req, sa)
 	if err != nil {
-		return
+		return err
 	}
-	info.Response, err = http.DefaultClient.Do(info.Request)
-	if err != nil {
-		return
-	}
-	resp, err = si.postHandle(c, req, info, nexter)
-	if err != nil {
-		return
-	}
-	if info.Response.StatusCode != http.StatusOK {
-		return resp, ErrStatusNotOk
-	}
-	return
+	sa.Response, err = template.Client.Do(sa.Request)
+	return template.decoder(c, resp, sa)
 }
 
-func (si *SingleAttemptInterceptor) preHandle(c context.Context, req any, info *SingleAttempt, nexter base.Nexter[*SingleAttempt]) (resp any, err error) {
-	if req == nil || info.Request.Body != nil {
-		return
+func (template *HttpTemplate) encoder(c context.Context, req any, sa *SingleAttempt) error {
+	if template.Encoder != nil {
+		return template.Encoder(c, req, sa)
 	}
-	contentType := info.Request.Header.Get("Content-Type")
-	if len(contentType) == 0 {
-		switch realReq := req.(type) {
-		case string:
-			info.Request.Body = io.NopCloser(strings.NewReader(realReq))
-		case []byte:
-			info.Request.Body = io.NopCloser(bytes.NewReader(realReq))
-		default:
-			buf, err := json.Marshal(req)
-			if err != nil {
-				return nil, err
-			}
-			info.Request.Body = io.NopCloser(bytes.NewReader(buf))
-		}
-		return
+	if req == nil || sa.Request.Body != nil {
+		return nil
 	}
-	return
-}
-
-func (si *SingleAttemptInterceptor) postHandle(c context.Context, req any, info *SingleAttempt, nexter base.Nexter[*SingleAttempt]) (resp any, err error) {
-	if info.Resp == nil {
-		return
-	}
-	buf, err := io.ReadAll(info.Response.Body)
-	info.Response.Body.Close()
-	if err != nil {
-		return
-	}
-	contentType := info.Response.Header.Get("Content-Type")
-	switch mimeType := GetMimeType(contentType); mimeType {
-	case "application/json":
-		err = json.Unmarshal(buf, info.Resp)
+	switch realReq := req.(type) {
+	case string:
+		sa.Request.Body = io.NopCloser(strings.NewReader(realReq))
+	case []byte:
+		sa.Request.Body = io.NopCloser(bytes.NewReader(realReq))
+	default:
+		buf, err := json.Marshal(req)
 		if err != nil {
-			return
+			return base.New(codes.InvalidArgument, "UnsupportRequestType").SetMsg(err.Error()).Err()
 		}
-	case "text/plain", "text/html":
-		switch realResp := info.Resp.(type) {
-		case *string:
-			*realResp = string(buf)
-		case *[]byte:
-			*realResp = buf
-		}
+		sa.Request.Body = io.NopCloser(bytes.NewReader(buf))
+		sa.Request.Header.Set("Content-Type", "application/json")
 	}
-	return info.Resp, nil
+	// goway retry support for http2
+	sa.Request.GetBody = func() (io.ReadCloser, error) {
+		return sa.Request.Body, nil
+	}
+	return nil
 }
 
-func GetMimeType(contentType string) string {
-	i := 0
-	for ; i < len(contentType); i++ {
-		if contentType[i] == ';' {
-			break
+func (template *HttpTemplate) decoder(c context.Context, resp any, sa *SingleAttempt) error {
+	if resp == nil {
+		return nil
+	}
+	if template.Decoder != nil {
+		return template.Decoder(c, sa)
+	}
+	buf, err := io.ReadAll(sa.Response.Body)
+	sa.Response.Body.Close()
+	sa.RespBody = buf
+	if err != nil {
+		return err
+	}
+	switch realResp := resp.(type) {
+	case *string:
+		*realResp = string(buf)
+	case *[]byte:
+		*realResp = buf
+	default:
+		err := json.Unmarshal(buf, resp)
+		if err != nil {
+			return err
 		}
 	}
-	return contentType[0:i]
+	return nil
 }
-
-var ErrStatusNotOk = errors.New("httpclient: status code not ok")
-
-var defaultSingleAttemptInterceptors = base.Chain[*SingleAttempt]{NewSingleAttemptInterceptor()}
--- httpcli/single_attempt_test.go
+++ httpcli/single_attempt_test.go
@@ -2,22 +2,14 @@ package httpcli
 
 import (
 	"context"
-	"fmt"
 	"github.com/stretchr/testify/require"
 	"net/http"
-	"net/http/httptrace"
 	"testing"
 )
 
 func TestSingleAttempt_Basic(t *testing.T) {
-	trace := &httptrace.ClientTrace{
-		GotConn: func(info httptrace.GotConnInfo) {
-			fmt.Printf("%s\n", info.Conn.RemoteAddr().String())
-		},
-	}
-	c := httptrace.WithClientTrace(context.Background(), trace)
-	httpReq, _ := http.NewRequestWithContext(c, http.MethodGet, "https://www.baidu.com", nil)
-	httpResp, err := http.DefaultClient.Do(httpReq)
+	sa := NewSingleAttempt(context.Background(), http.MethodGet, "https://www.baidu.com")
+	buf, err := Do[string](sa, "")
 	require.NoError(t, err)
-	_ = httpResp
+	require.Contains(t, buf, "baidu")
 }
