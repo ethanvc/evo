4509d8a97e04a3b6e64323ceda5cd33f5a15363f
vc <xht_xht@qq.com>
2026-05-21 16:11:30 +0800

    add code

--- httpmux/httpmux.go
+++ httpmux/httpmux.go
@@ -1,6 +1,11 @@
 package httpmux
 
+import "net/http"
+
 type Pattern = pattern
 
-func (mux *ServeMux) MatchPattern(method, host, p string) (h Handler, patStr string, pattern *Pattern, matches []string) {
+func (mux *ServeMux) MatchPattern(method, host, p string) (pattern *Pattern, matches []string) {
+	req, _ := http.NewRequest(method, host+p, nil)
+	_, _, pattern, matches = mux.findHandler(req)
+	return pattern, matches
 }
