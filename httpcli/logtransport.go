7a6262ca9f9424b3fee426bbc8f596a66e7d7cc8
ethanvc <xht_xht@qq.com>
2026-05-28 00:09:39 +0800

    add code

--- httpcli/logtransport.go
+++ httpcli/logtransport.go
@@ -2,6 +2,8 @@ package httpcli
 
 import (
 	"net/http"
+
+	"github.com/ethanvc/evo/obs"
 )
 
 type LogTransport struct {
@@ -12,14 +14,23 @@ func (t *LogTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	req = t.init(req)
 	resp, err := t.next.RoundTrip(req)
 	// here does not catch panic to let upper layer catch and report it
-	t.report(resp, err)
+	t.report(req, resp, err)
 	return resp, err
 }
 
 func (t *LogTransport) init(req *http.Request) *http.Request {
+	ctx := obs.WithSpanContext(req.Context(), &obs.SpanConfig{
+		Method: req.URL.Path,
+	})
+	req = req.WithContext(ctx)
 	return req
 }
 
-func (t *LogTransport) report(resp *http.Response, err error) {
-
+func (t *LogTransport) report(req *http.Request, resp *http.Response, err error) {
+	obsCtx := obs.GetObsContext(req.Context())
+	if err != nil {
+		obsCtx.AccessLogReport(req.Context(), err, req, resp, nil)
+		return
+	}
+	obsCtx.AccessLogReport(req.Context(), nil, req, resp, nil)
 }
